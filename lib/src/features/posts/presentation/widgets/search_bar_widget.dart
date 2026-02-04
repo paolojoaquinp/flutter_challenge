4f093400ef15bc1003beb6a6a23c80192bd5c7b2
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2026-02-04 02:50:53 -0400

    feat: search input debouncer

--- lib/src/features/posts/presentation/widgets/search_bar_widget.dart
+++ lib/src/features/posts/presentation/widgets/search_bar_widget.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import '../../../../core/design/tokens/palette.dart';
+import 'package:flutter_challenge/src/core/design/tokens/palette.dart';
 
-class SearchBarWidget extends StatelessWidget {
+
+class SearchBarWidget extends StatefulWidget {
   final ValueChanged<String> onChanged;
   final String hintText;
 
@@ -12,6 +14,26 @@ class SearchBarWidget extends StatelessWidget {
   });
 
   @override
+  State<SearchBarWidget> createState() => _SearchBarWidgetState();
+}
+
+class _SearchBarWidgetState extends State<SearchBarWidget> {
+  Timer? _debounce;
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  void _onSearchChanged(String query) {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      widget.onChanged(query);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
@@ -19,10 +41,10 @@ class SearchBarWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
       ),
       child: TextField(
-        onChanged: onChanged,
+        onChanged: _onSearchChanged,
         decoration: InputDecoration(
-          hintText: hintText,
-          prefixIcon: const Icon(Icons.search, color: Palette.primary),
+          hintText: widget.hintText,
+          prefixIcon: const Icon(Icons.search, color: Palette.brandPrimary),
           border: InputBorder.none,
           contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         ),
